835c5cb7eaaed1417d8c773af265db669620cbee
TomohikoK <Kinebuchi.Tomohiko@gmail.com>
2018-05-09 22:22:05 +0900

    Fix function name

--- slide/snippet/list_map.py
+++ slide/snippet/list_map.py
@@ -1,8 +1,8 @@
 from typing import List
 
-def length_of_string(arg: str) -> int:
+def length_of_str(arg: str) -> int:
     return len(arg)
 
 x: List[str] = ['a', 'bb', 'ccc']
-y: List[int] = list(map(length_of_string, x))
+y: List[int] = list(map(length_of_str, x))
 assert y == [1, 2, 3]
